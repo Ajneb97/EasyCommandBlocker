3443b8388f2ca89428b057781a56e56d7a6fe624
Ajneb97 <ajneb97@gmail.com>
2022-06-05 12:10:30 -0400

    1.4.2

--- src/main/java/ecb/ajneb97/core/model/GlobalVariables.java
+++ src/main/java/ecb/ajneb97/core/model/GlobalVariables.java
@@ -2,6 +2,6 @@ package ecb.ajneb97.core.model;
 
 public class GlobalVariables {
 
-    public static final String bungeeMainChannel = "easycommandblocker:channel";
+    public static final String bungeeMainChannel = "ecb:channel";
     public static final String bungeeActionsSubChannel = "ActionsSubChannel";
 }
