675fd472988a41082701cf3bb90705b2cb99c90b
Ajneb97 <ajneb97@gmail.com>
2023-06-08 08:48:12 -0400

    1.8.1

--- src/main/java/ecb/ajneb97/spigot/utils/OtherUtils.java
+++ src/main/java/ecb/ajneb97/spigot/utils/OtherUtils.java
@@ -12,7 +12,8 @@ public class OtherUtils {
 
     public static boolean serverIsNew() {
         if(Bukkit.getVersion().contains("1.16") || Bukkit.getVersion().contains("1.17")
-                || Bukkit.getVersion().contains("1.18") || Bukkit.getVersion().contains("1.19")) {
+                || Bukkit.getVersion().contains("1.18") || Bukkit.getVersion().contains("1.19")
+                || Bukkit.getVersion().contains("1.20")) {
             return true;
         }else {
             return false;
@@ -23,7 +24,7 @@ public class OtherUtils {
         if(Bukkit.getVersion().contains("1.13") || Bukkit.getVersion().contains("1.14") ||
                 Bukkit.getVersion().contains("1.15") || Bukkit.getVersion().contains("1.16")
                 || Bukkit.getVersion().contains("1.17") || Bukkit.getVersion().contains("1.18")
-                || Bukkit.getVersion().contains("1.19")) {
+                || Bukkit.getVersion().contains("1.19") || Bukkit.getVersion().contains("1.20")) {
             return false;
         }else {
             return true;
