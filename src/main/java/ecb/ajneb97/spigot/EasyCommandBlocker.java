30a3dc47b105e3e4e20cc9a5d3035d1df5fe684f
Ajneb97 <ajneb97@gmail.com>
2024-08-11 12:04:59 -0400

    1.9.2

--- src/main/java/ecb/ajneb97/spigot/EasyCommandBlocker.java
+++ src/main/java/ecb/ajneb97/spigot/EasyCommandBlocker.java
@@ -62,6 +62,7 @@ public class EasyCommandBlocker extends JavaPlugin {
                 serverVersion = ServerVersion.v1_20_R4;
                 break;
             case "1.21":
+            case "1.21.1":
                 serverVersion = ServerVersion.v1_21_R1;
                 break;
             default:
