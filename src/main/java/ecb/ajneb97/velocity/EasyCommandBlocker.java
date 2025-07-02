7234ddc931fa36d1b033dafb97d80578e45cd961
Ajneb97 <ajneb97@gmail.com>
2025-07-02 17:00:54 -0400

    1.14.2

--- src/main/java/ecb/ajneb97/velocity/EasyCommandBlocker.java
+++ src/main/java/ecb/ajneb97/velocity/EasyCommandBlocker.java
@@ -17,7 +17,7 @@ import java.nio.file.Path;
 import java.util.logging.Logger;
 
 @Plugin(id = "easycommandblocker", name = "EasyCommandBlocker",
-        version = "1.14.1", authors = {"Ajneb97"})
+        version = "1.14.2", authors = {"Ajneb97"})
 public class EasyCommandBlocker {
 
     private final ProxyServer server;
